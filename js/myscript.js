d422f8344d51186e9dff43be4e14f01dc30e0be6
darulmuqimin <darulmuqimin.official@gmail.com>
2022-03-14 01:08:19 +0700

    update required
    gallery | index

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,8 @@
-    const santris = document.getElementById('santri');
+   
+   const santris = document.getElementById('santri');
     const thumbns = document.querySelector('.thumbn');
    
-    const thumbnailss= document.querySelectorAll('.thumbnails rounded');
+    const thumbnailss= document.querySelectorAll('.thumbnailss rounded');
     santris.addEventListener('click', function(e) {
         if ( e.target.className == 'thumbnailss rounded' ) {
             thumbns.src = e.target.src;
